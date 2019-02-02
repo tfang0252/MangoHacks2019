aeeba8f5b3135b5e15b9a53fc158d37cc5f71359
Unknown <tfang0252@eagle.fgcu.edu>
2019-02-01 19:39:51 -0500

    Everry thang is setup!@#!@#

--- MangoHacks2019/MangoHacks2019/AppDelegate.swift
+++ MangoHacks2019/MangoHacks2019/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection10.bundle")?.load()
+        
+        FirebaseApp.configure()
         return true
     }
 
--- MangoHacks2019/MangoHacks2019/ViewController.swift
+++ MangoHacks2019/MangoHacks2019/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    
+    @objc func injected() {
+        viewDidLoad()
+        viewWillAppear(true)
+        viewDidAppear(true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
